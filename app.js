a9c1f8e2dd102a6ba093b0259b925e1cdd4480c3
shem4soul@gmail.com <shem4soul@gmail.com>
2025-05-23 14:40:45 +0100

    chore: update mongoose dependency to version 8.15.0

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-
+require('dotenv').config()
 
 const express = require('express')
 const app = express()
 
+
+const connectDB = require('./db/connect')
+
 const port = process.env.PORT || 5000
 const start = async () => {
     try {
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, console.log(`Server is listening on port ${port}`));
         
     } catch (error) {
