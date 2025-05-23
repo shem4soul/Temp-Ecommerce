9af0a756cfbe743b5f173c86e0d83e790128c607
shem4soul@gmail.com <shem4soul@gmail.com>
2025-05-23 14:44:14 +0100

    feat: add root route to serve e-commerce API message

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-
+//database
 const connectDB = require('./db/connect')
+
+app.get('/', (req, res) =>{
+    res.send('e-commerce api')
+})
 
 const port = process.env.PORT || 5000
 const start = async () => {
