4f690536fe335c11c236b7b366078159eb475abb
shem4soul@gmail.com <shem4soul@gmail.com>
2025-05-23 16:10:29 +0100

    feat: add morgan middleware for logging HTTP requests

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ require('express-async-errors')
 const express = require('express')
 const app = express()
 
+//rest of the packages
+const morgan = require('morgan')
+
+
 //database
 const connectDB = require('./db/connect')
 
@@ -11,6 +15,7 @@ const connectDB = require('./db/connect')
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
+app.use(morgan('tiny'))
 app.use(express.json())
 
 app.get('/', (req, res) =>{
