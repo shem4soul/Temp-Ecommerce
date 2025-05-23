f18961c8db5a325b1aa04b79e8c80867cc9f3af8
shem4soul@gmail.com <shem4soul@gmail.com>
2025-05-23 16:19:01 +0100

    fix: correct typo in 'required' field for email in User schema

--- models/User.js
+++ models/User.js
@@ -1,3 +1,27 @@
 const mongoose = require('mongoose')
 
-const UserSchema
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please provide name'],
+        minlength: 3,
+        maxlength: 50,
+        },
+        email: {
+            type: String,
+            requied: [true, 'please provide email']
+        },
+        password: {
+            type: String,
+            required: [true, 'Please provide password'],
+            minlength: 6,
+        },
+        role: {
+            type: String,
+            enum: ['admin', 'user'],
+            default: 'user',
+        },
+
+})
+
+module.exports = mongoose.model('User', UserSchema)
