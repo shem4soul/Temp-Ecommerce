b461b77da2c5c2381c118d768ff143274d046ef2
shem4soul@gmail.com <shem4soul@gmail.com>
2025-05-25 16:28:05 +0100

    feat: enhance user registration to include JWT token generation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,8 @@
 const User = require('../models/User')
 const {StatusCodes} = require('http-status-codes')
 const CustomError = require('../errors')
+const jwt = require('jsonwebtoken')
+
 
 const register = async (req, res)  => {
     const {name, email, password} = req.body
@@ -15,7 +17,10 @@ const register = async (req, res)  => {
   const role = isFirstAccount ? 'admin' : 'user'
  
 const user = await User.create({name,email, password, role})
-res.status(StatusCodes.CREATED).json({user})
+const tokenUser = {name: user.name, userId: user._id, role: user.role}
+const token = jwt.sign(tokenUser, 'jwtSecret', {expiresIn:'1d'})
+
+res.status(StatusCodes.CREATED).json({user: tokenUser, token})
 }
 
 const login = async (req, res) => {
