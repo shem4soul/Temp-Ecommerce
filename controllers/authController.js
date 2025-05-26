185a2766152069b34f7551ed95c141bc074e069c
shem4soul@gmail.com <shem4soul@gmail.com>
2025-05-26 04:21:18 +0100

    feat: implement attachCookiesToResponse utility for handling JWT cookies in authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,7 @@ const {StatusCodes} = require('http-status-codes')
 const CustomError = require('../errors')
 
 
-const {createJWT} = require('../utils')
+const {attachCookiesToResponse} = require('../utils')
 
 
 const register = async (req, res)  => {
@@ -20,17 +20,8 @@ const register = async (req, res)  => {
  
 const user = await User.create({name,email, password, role})
 const tokenUser = {name: user.name, userId: user._id, role: user.role}
-
-const token = createJWT({payload: tokenUser})
-
-const oneDay = 1000 * 60 * 60 * 24
-
-res.cookie('token', token, {
-  httpOnly: true,
-  expires: new Date(Date.now() + oneDay)
-})
-
-res.status(StatusCodes.CREATED).json({user: tokenUser})
+attachCookiesToResponse({res, user:tokenUser})
+ //res.status(StatusCodes.CREATED).json({user: tokenUser})
 }
 
 const login = async (req, res) => {
