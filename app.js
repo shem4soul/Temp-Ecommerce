63e9af021287f54f7651e011213d2271e81a8525
shem4soul@gmail.com <shem4soul@gmail.com>
2025-05-26 04:41:31 +0100

    fix: update cookie-parser middleware to use JWT_SECRET and log signedCookies in API response

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const errorHandlerMiddleware = require('./middleware/error-handler')
 
 app.use(morgan('tiny'))
 app.use(express.json())
-app.use(cookieParser())
+app.use(cookieParser(process.env.JWT_SECRET))
 
 app.get('/', (req, res) =>{
     
@@ -29,7 +29,7 @@ app.get('/', (req, res) =>{
 })
 
 app.get("/api/v1", (req, res) => {
-  console.log(req.cookies);
+  console.log(req.signedCookies);
   res.send('e-commerce api')
   
 });
