ccfe20197c7378d3d6ab0028ab987be27da6e3d9
shem4soul@gmail.com <shem4soul@gmail.com>
2025-05-30 17:19:59 +0100

    update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,22 @@
-const getAllUsers = (req, res) => {
-  res.send('get all users');
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');   
+const CustomError = require('../errors');
+
+
+const getAllUsers = async (req, res) => {
+const users = await User.find({role:'user'}).select('-password'); // Exclude password field
+  if (!users) {
+    throw new CustomError.NotFoundError('No users found');
+  }
+  res.status(StatusCodes.OK).json({ users, count: users.length });
 }
 
-const getSingleUser = (req, res) => {
-  res.send('get single user');
+const getSingleUser = async (req, res) => {
+ const user = await User.findOne({_id: req.params.id}).select('-password'); // Exclude password field
+ if (!user) {
+    throw new CustomError.NotFoundError(`No user found with id: ${req.params.id}`);
+  }
+  res.status(StatusCodes.OK).json({ user });  
 }
 
 const showCurrentUser = (req, res) => {
