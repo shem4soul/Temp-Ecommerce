14c6e7a5dd1a0751fee52e6616917429dcac8586
shem4soul@gmail.com <shem4soul@gmail.com>
2025-06-05 19:27:36 +0100

    fix: improve token validation in authentication middleware

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -3,3 +3,19 @@ const { isTokenValid } = require('../utils');
 
 
 const authenticateUser = async (req, res, next) => {
+    const token = req.signedCookies.token
+    
+    if(!token) {
+      console.log('error: No token provided');
+             
+    }
+    console.log('token present');
+    next();
+    
+    
+}
+
+
+module.exports = {
+    authenticateUser
+}
