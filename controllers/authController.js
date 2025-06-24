49f4ad5ecf544db3ee7b1de4449017a936018847
shem4soul@gmail.com <shem4soul@gmail.com>
2025-06-24 20:23:54 +0100

    feat: refactor tokenUser creation to use createTokenUser utility in register and login functions

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,8 +2,7 @@ const User = require('../models/User')
 const {StatusCodes} = require('http-status-codes')
 const CustomError = require('../errors')
 
-
-const {attachCookiesToResponse} = require('../utils')
+const {attachCookiesToResponse, createTokenUser} = require('../utils')
 
 
 const register = async (req, res)  => {
@@ -19,7 +18,8 @@ const register = async (req, res)  => {
   const role = isFirstAccount ? 'admin' : 'user'
  
 const user = await User.create({name,email, password, role})
-const tokenUser = {name: user.name, userId: user._id, role: user.role}
+const tokenUser = createTokenUser(user)
+ // attach cookies to response
 attachCookiesToResponse({res, user:tokenUser})
  res.status(StatusCodes.CREATED).json({user: tokenUser})
 }
@@ -37,7 +37,7 @@ const isPasswordCorrect = await user.comparePassword(password)
 if (!isPasswordCorrect) {
     throw new CustomError.UnauthenticatedError('Invalid Credentials')     
     }
-const tokenUser = {name: user.name, userId: user._id, role: user.role}  
+const tokenUser = createTokenUser(user)
 attachCookiesToResponse({res, user: tokenUser})
 res.status(StatusCodes.CREATED).json({user: tokenUser})
 }
