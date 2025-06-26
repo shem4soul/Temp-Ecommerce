91e3cda0a3978a17630a7d55cd5dde88e710b158
shem4soul@gmail.com <shem4soul@gmail.com>
2025-06-26 17:35:17 +0100

    feat: add checkPermissions utility function for user access validation

---
+++ utils/checkPermisions.js
@@ -0,0 +1,11 @@
+const CustomError = require('../errors');
+
+const checkPermissions = (requestUser, resourceUserId) => {
+    console.log(requestUser);
+    console.log(resourceUserId);
+    console.log(typeof resourceUserId);
+}
+
+module.exports = checkPermissions;
+
+    
