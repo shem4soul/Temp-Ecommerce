c93c2686e84ae4819c7bd3e86da4fdba15211ba8
shem4soul@gmail.com <shem4soul@gmail.com>
2025-06-26 18:42:04 +0100

    feat: extend ProductSchema with image, category, and company fields

--- models/product.js
+++ models/product.js
@@ -15,4 +15,21 @@
         required: [true, 'Please provide product description'],
         maxlength: [1000, 'Description cannot be more than 1000 characters']
     },
+    image: {
+        type: String,
+        default: '/uploads/example.jpeg'
+    },
+    category: {
+        type: String,
+        required: [true, 'Please provide product category'],
+        enum: ['office', 'kitchen', 'bedroom']
+    },
+    company: {
+        type: String,
+        required: [true, 'Please provide company'],
+        enum: {
+            values: ['ikea', 'liddy', 'marcos'],
+            message: '{VALUE} is not supported'
+        }
+    },
  })
