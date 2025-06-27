1e15b3e8522e9236536c20495de6635c992e5239
shem4soul@gmail.com <shem4soul@gmail.com>
2025-06-27 19:46:26 +0100

    feat: implement getAllProducts functionality to retrieve all products

--- controllers/productController.js
+++ controllers/productController.js
@@ -9,7 +9,9 @@ const createProduct = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-    res.send('get all products');
+    const products = await Product.find({})
+    res.status(StatusCodes.OK).json({products, count: products.length})
+ 
 }
 
 const getSingleProduct = async (req, res) => {
