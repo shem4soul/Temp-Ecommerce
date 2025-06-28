4ffad70fd839ce231362081903e6bb0297176456
shem4soul@gmail.com <shem4soul@gmail.com>
2025-06-28 20:05:08 +0100

    feat: add Review model with schema and unique index for user and product

---
+++ models/Review.js
@@ -0,0 +1,37 @@
+const { required } = require('joi')
+const mongoose = require('mongoose')
+
+const ReviewSchema = mongoose.Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: [true, 'please provide rating'],
+    },
+   title: {
+    type: String,
+    trim: true,
+    required: [true, 'please provide review title']
+   },
+   comment: {
+    type: String,
+    required: [true, 'please provide review text'],
+   },
+   user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: true,
+   },
+   product: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Product',
+    required: true,
+   },
+},
+   {timestamps: true}
+
+)
+
+ReviewSchema.index({product: 1, user: 1}, {unique: true})
+
+module.exports = mongoose.model('Review', ReviewSchema)
