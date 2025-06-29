09c399a8ebccae0e137a2ace11066bec56a4fa18
shem4soul@gmail.com <shem4soul@gmail.com>
2025-06-29 05:19:42 +0100

    feat: finalize createReview function to include user assignment and response

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -24,6 +24,10 @@ const createReview = async (req, res) => {
       )
     }
 
+    req.body.user = req.user.userId;
+    const review = await Review.create(req.body)
+    res.status(StatusCodes.CREATED).json({review})
+
 }
 
 const getAllReviews = async (req, res) => {
