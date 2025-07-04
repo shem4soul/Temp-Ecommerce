1de79bd5d4b54c3bd403a56855995ed7320a16a7
shem4soul@gmail.com <shem4soul@gmail.com>
2025-07-04 20:57:34 +0100

    feat: enhance createOrder function to improve validation and error handling for cart items, tax, and shipping fee

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -5,20 +5,65 @@ const CustomError = require("../errors");
 const { checkPermissions} = require("../utils");
 
 
+// const createOrder = async (req, res) => {
+//   const {items:cartItems, tax, shippingFee} = req.body
+
+//   if (!items || items.length < 1) {
+//     throw new CustomError.BadRequestError('No cart items provided')
+//   }
+//   if (!tax || !shippingFee) {
+//     throw new CustomError.BadRequestError(
+//       'Please provide tax and shipping fee'
+//     )
+//   }
+
+//   let orderItems = []
+//   let subtotal = 0
+
+//   for (const item of cartItems) {
+//     const dbProduct = await Product.findOne({_id: item.product})
+//     if (!dbProduct) { 
+//       throw new CustomError.NotFoundError(
+//         `No product with id : ${item.product}`
+//       )
+//     }
+//     const {name, price , image, _id } = dbProduct
+//     console.log(name, price, image)
+//   }
+
+//   res.send('create order')
+
+// };
+
+
 const createOrder = async (req, res) => {
-  const {items:cartItems, tax, shippingFee} = req.body
+  const { items: cartItems, tax, shippingFee } = req.body;
 
   if (!cartItems || cartItems.length < 1) {
-    throw new CustomError.BadRequestError('No cart items provided')
+    throw new CustomError.BadRequestError('No cart items provided');
   }
-  if (!tax || !shippingFee) {
-    throw new CustomError.BadRequestError(
-      'Please provide tax and shipping fee'
-    )
-  }
-  res.send('create order')
 
+  if (!tax || !shippingFee) {
+    throw new CustomError.BadRequestError('Please provide tax and shipping fee');
+  }
+
+  let orderItems = [];
+  let subtotal = 0;
+
+  for (const item of cartItems) {
+    const dbProduct = await Product.findOne({ _id: item.product });
+
+    if (!dbProduct) {
+      throw new CustomError.NotFoundError(`No product with id: ${item.product}`);
+    }
+
+    const { name, price, image, _id } = dbProduct;
+    console.log(name, price, image);
+  }
+
+  res.send('create order');
 };
+
 
 const getOrder = async (req, res) => {
   const { orderId } = req.params;
