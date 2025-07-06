6cd0b2221b014ba22f9d8db1c208dc6174bad768
shem4soul@gmail.com <shem4soul@gmail.com>
2025-07-06 20:04:32 +0100

    fix: correct Order model import in orderControllers.js

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -1,40 +1,8 @@
-const Order = require('../models/Review')
+const Order = require('../models/Order')
 const Product = require('../models/product')
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
 const { checkPermissions} = require("../utils");
-
-
-// const createOrder = async (req, res) => {
-//   const {items:cartItems, tax, shippingFee} = req.body
-
-//   if (!items || items.length < 1) {
-//     throw new CustomError.BadRequestError('No cart items provided')
-//   }
-//   if (!tax || !shippingFee) {
-//     throw new CustomError.BadRequestError(
-//       'Please provide tax and shipping fee'
-//     )
-//   }
-
-//   let orderItems = []
-//   let subtotal = 0
-
-//   for (const item of cartItems) {
-//     const dbProduct = await Product.findOne({_id: item.product})
-//     if (!dbProduct) { 
-//       throw new CustomError.NotFoundError(
-//         `No product with id : ${item.product}`
-//       )
-//     }
-//     const {name, price , image, _id } = dbProduct
-//     console.log(name, price, image)
-//   }
-
-//   res.send('create order')
-
-// };
-
 
 const createOrder = async (req, res) => {
   const { items: cartItems, tax, shippingFee } = req.body;
